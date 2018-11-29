2960ba89555674f62c652c1f0cdaafd55de8db37
josé miguel angel amaro hernández <jma_hernandez@hotmail.com>
2018-11-29 17:03:48 -0600

    Add unmatched route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # resources :posts
 
+  root 'posts#index'
+
   namespace :api, :defaults => { :format => :json } do
 		namespace :v1 do
 			resources :pages
 		end
 	end
+
+	get '*unmatchedroute', to: redirect('/')
+
 end
